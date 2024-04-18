2a456c6e371c92b7b052e3ae3e53ebd380e97446
Rofy-Ray <okyereforsonr@berea.edu>
2024-04-18 03:39:48 -0500

    updated handler funcs loop

--- services.py
+++ services.py
@@ -1995,7 +1995,6 @@ def manage_chatbot(text: str, number: str, messageId: str, name: str, numberId: 
         "company names": handle_company_names,
         "vto options": handle_vto_options,
         "vto selfie": handle_vto_selfie,
-        # "else": handle_else_condition,
     }
     
     params = {
@@ -2009,6 +2008,7 @@ def manage_chatbot(text: str, number: str, messageId: str, name: str, numberId: 
         # If the keyword is "greetings" and the text is a greeting
         if keyword == "greetings" and any(greeting in text for greeting in greetings):
             response_list = handler(text, number, messageId, response_list)
+            break
 
         # If the keyword is the stripped text
         elif keyword == stripped_text:
@@ -2016,26 +2016,31 @@ def manage_chatbot(text: str, number: str, messageId: str, name: str, numberId: 
                 response_list = handler(stripped_text, number, messageId, response_list, **params[handler])
             else:
                 response_list = handler(stripped_text, number, messageId, response_list)
+            break
             
         # If the keyword is the text
         elif keyword == text:
             response_list = handler(text, number, response_list)
+            break
 
         # If the keyword is "digit text" and the text is a digit
         elif keyword == "digit text" and text.isdigit():
             response_list = handler(text, number, messageId, numberId, response_list)
+            break
 
         # If the keyword is "company names" and the text is a company name
         elif keyword == "company names" and any(
             option in text for option in recs_data["company_names"]
         ):
             response_list = handler(text, number, messageId, name, response_list)
+            break
 
         # If the keyword is "vto options" and the text is a VTO option
         elif keyword == "vto options" and any(
             option in text for option in feats[last_vto_type[number][0]].keys()
         ):
             response_list = handler(text, number, messageId, response_list)
+            break
 
         # If the keyword is "vto selfie" and the text is a VTO selfie option
         elif keyword == "vto selfie" and any(
@@ -2045,16 +2050,7 @@ def manage_chatbot(text: str, number: str, messageId: str, name: str, numberId: 
             ].keys()
         ):
             response_list = handler(text, number, response_list)
-
-        # If none of the above conditions are met, continue to the next keyword and handler
-        else:
-            # continue
-            res = handle_else_condition(text, number, messageId, response_list, chat_history)
-            response_list = res[0]
-            chat_history = res[1]
-
-        # If one of the above conditions is met, break the loop
-        break
+            break
     # If none of the above conditions are met, use the "else" handler
     else:
         res = handle_else_condition(text, number, messageId, response_list, chat_history)
